5fe1018cdae3c1ebcf7deb9c57ba970d6f2b4911
RafaelFreitas <raf.mec.ba@gmail.com>
2022-09-07 10:45:14 -0300

    S5-13 final

---
+++ script.js
@@ -0,0 +1,55 @@
+let carro = {
+    marca: 'Ford',
+    ano: 2010,
+    modelo: 'KA',
+    cor: 'Azul',
+    donos: []   
+  }
+
+
+// -----------------Ex 1-----------------
+let pessoa = {
+    nome: "Rafael Freitas",
+    AnoNascimento: 1989,
+    cpf: "02288692502",
+    cidade: "Salvador",
+    estado: "Ba",
+    lodradouro: "Rua marcio Batista, 104, Stiep"   
+  }
+
+// -----------------Ex 2-----------------
+let escola = {
+    nome: "Doçúcar",
+    cnpj: 40355956000120,
+    areaAtuacao: "Gastronomia",
+    cidade: "Salvador",
+    estado: "Ba",
+    lodradouro: "Rua marcio Batista, 104, Stiep", 
+    curso: ["Confeitaria","Massas","Modelagem de Pasta Americana"]  
+  }
+
+// -----------------Ex 3-----------------
+let curso = {
+    nome:"Engenharia Mecânica",
+    duraçãoAnos:5,
+    turma:2,
+    modulos: ["m1","m2","m3","m4","m5"]
+}
+
+// -----------------Ex 4-----------------
+let endereco = {
+    cidade: "Salvador",
+    estado: "Ba",
+    lodradouro: "Rua marcio Batista, 104, Stiep",
+    cep:41770015
+}
+
+// -----------------Ex 5-----------------
+let turma = {
+    professores: ["Edna", "Paulo", "Sidney", "Renata"],
+    alunos: ["Rafael","Anajúlia","Ovídio","Joana"],
+    dataInicio: 07/2022,
+    previsaoFormatura:07/2023
+}
+
+
